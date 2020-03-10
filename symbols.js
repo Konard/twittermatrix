4611c961e8ec4f22fddafe72d8d149080925d80e
Konstantin Diachenko <drakonard@gmail.com>
2020-03-10 17:00:24 +0300

    Added unistring library. Better symbols rendering (Hindi and other languages supported, faces are replaced due to Google Chrome behavior, hearts rendered as text).

--- symbols.js
+++ symbols.js
@@ -1,74 +1,48 @@
-var faceReplacement = '\u2687'; // Replacement character for faces 
+﻿var faceReplacement = '\u2687'; // Replacement character for faces 
 var renderAsText = '\uFE0E'; // VARIATION SELECTOR-15 'text style'
 var renderAsImage = '\uFE0F'; // VARIATION SELECTOR-16 'emoji style'
+var zeroSymbol = "0".charCodeAt(0);
+var nineSymbol = "9".charCodeAt(0);
 
-Number.prototype.inRange = function(min, max)
-{
+Number.prototype.inRange = function(min, max) {
 	return this >= min && this <= max;
 }
 
-function getSymbols(string)
-{
-	var index = 0;
-	var length = string.length;
+String.prototype.isFace = function () {
+	if (this.length >= 2) {
+		var firstCode = this.charCodeAt(0);
+		var secondCode = this.charCodeAt(1);
+		return (firstCode == 0xD83E && (secondCode.inRange(0xDD10, 0xDD17) || secondCode.inRange(0xDD20, 0xDD2F) || secondCode.inRange(0xDD70, 0xDD7A) || secondCode.inRange(0xDDD0, 0xDDD4)))
+			|| (firstCode.inRange(0xD83D, 0xDBFF) && secondCode.inRange(0xDE00, 0xDE44));
+	}
+	return false;
+}
+
+String.prototype.isSquareWithDigit = function () {
+	return this.length == 3 && this.charCodeAt(0).inRange(zeroSymbol, nineSymbol) && this.charCodeAt(1) == 0xFE0F && this.charCodeAt(2) == 0x20E3;
+}
+
+String.prototype.isHeart = function () {
+	return this.length == 2 && this.charCodeAt(0) == 0xD83D && (this.charCodeAt(1) == 0xDDA4 || this.charCodeAt(1).inRange(0xDC93, 0xDC9F));
+}
+
+function getSymbols(string) {
+	var unistring = Unistring(string.normalize());
 	var output = [];
-	for (; index < length; index++)
-	{
-		if ((index + 1) < length)
-		{
-			var charCode = string.charCodeAt(index);
-			if (charCode == 0xFE0F)
-			{
-				output.push(renderAsText);
-				continue;
+	for (var i = 0; i < unistring.length; i++) {
+		var cluster = unistring.clusterAt(i);
+		if (cluster.indexOf(renderAsImage) >= 0) {
+			if (cluster.isSquareWithDigit()) {
+				cluster = cluster.charAt(0);
+			} else {
+				cluster = cluster.replace(renderAsImage, renderAsText);
 			}
-			if (charCode.inRange(0x0900, 0x097F) || charCode.inRange(0xA8E0, 0xA8FF)) // Devanagari support
-			{
-				var startIndex = index;
-				var nextChar = string.charAt(index + 1);
-				while(/\p{M}/gu.test(nextChar))
-				{
-					index++;
-					if ((index + 1) < length)
-						nextChar = string.charAt(index + 1);
-					else
-						break;
-				}
-				output.push(string.slice(startIndex, index + 1));
-				continue;
-			}
-			if (charCode.inRange(0xD83D, 0xDBFF)) // Override for EMOJI
-			{
-				charCode = string.charCodeAt(index + 1);
-				if (charCode.inRange(0xDE00, 0xDE4F))
-				{
-					output.push(faceReplacement);
-					index++;
-					continue;
-				}
-			}
-			if (charCode == 0xD83E) // Override for EMOJI
-			{
-				charCode = string.charCodeAt(index + 1);
-				if (charCode.inRange(0xDD10, 0xDD2F))
-				{
-					output.push(faceReplacement);
-					index++;
-					continue;
-				}
-			}
-			if (charCode.inRange(0xD800, 0xDBFF)) // Multi-character symbols (eg. UTF-32)
-			{
-				charCode = string.charCodeAt(index + 1);
-				if (charCode.inRange(0xDC00, 0xDFFF))
-				{
-					output.push(string.slice(index, index + 2));
-					index++;
-					continue;
-				}
-			}
+		} else if (cluster.isFace()) {
+			cluster = faceReplacement;
+		} else if (cluster.isHeart()) {
+			cluster = cluster + renderAsText;
 		}
-		output.push(string.charAt(index));
+		output.push(cluster);
 	}
 	return output;
 }
